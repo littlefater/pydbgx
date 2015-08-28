92131afd25aadb1c26dbc59f3eda47c0ad59cc33
z3r0zh0u <littlefater2012@gmail.com>
2015-08-28 17:18:46 +0800

    Update class PyDbgX

--- pydbgx.py
+++ pydbgx.py
@@ -6,30 +6,44 @@ Python wrapper for Windows Debugger Engine API.
 import platform
 
 from ctypes import *
-#from comtypes.gen import DbgEng
+from comtypes.hresult import S_OK
+
+try:
+    from comtypes.gen import DbgEng
+except ImportError:
+    from comtypes.client import GetModule
+    GetModule("helper/DbgEng.tlb")
+    from comtypes.gen import DbgEng
+
 
 if platform.architecture()[0] == '32bit':
     try:
         dbghelp = windll.LoadLibrary("lib/dbghelp.dll") 
         dbgeng = windll.LoadLibrary("lib/dbgeng.dll")
     except:
-        print 'Can not load dbghelp.dll and dbgeng.dll,  '
-else:
+        print 'Can not load dbghelp.dll and dbgeng.dll'
+elif platform.architecture()[0] == '64bit':
     try:
         dbghelp = windll.LoadLibrary("lib/dbghelp_x64.dll") 
         dbgeng = windll.LoadLibrary("lib/dbgeng_x64.dll")
-     except:
+    except:
+         print 'Can not load dbghelp.dll and dbgeng.dll'
+else:
+    raise Exception('Unsupported system.')
 
 
 DebugCreate = dbgeng.DebugCreate
+
 
 class PyDbgX:
 
     def __init__(self):
 
-        self.__debugClient = c_void_p()
-
-        #hr = DebugCreate(, self.__debugClient)
+        self.__debug_client = POINTER(DbgEng.IDebugClient)()
+        
+        hr = DebugCreate(byref(DbgEng.IDebugClient._iid_), byref(self.__debug_client))
+        if S_OK != hr:
+            raise Exception('DebugCreate() fail.')
         
 
 if __name__ == '__main__':
