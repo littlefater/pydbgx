984c7fe47d6e9bab45df0a2290f086adb6006a1f
z3r0zh0u <littlefater2012@gmail.com>
2015-09-14 15:19:10 +0800

    Update example httphook.py

--- examples/httphook.py
+++ examples/httphook.py
@@ -26,6 +26,7 @@ class MyDebugEventCallbacks(DebugEventCallbacks):
         super(DebugEventCallbacks, self).__init__()
         self.__mask = mask
         self.__pydbgx = None
+        self.__bplist = dict()
 
     def bind_pydbgx(self, pydbgx):
         """bind pydbgx"""
@@ -55,7 +56,7 @@ class MyDebugEventCallbacks(DebugEventCallbacks):
         Param = Bp.GetParameters()
         if Param.BreakType == DbgEng.DEBUG_BREAKPOINT_CODE:
             
-            print 'Breakpoint Hit on Address:', hex(Param.Offset)
+            print 'Breakpoint:', hex(Param.Offset)
             
             buffer_size = Param.OffsetExpressionSize + 1
             buffer = create_string_buffer(buffer_size)
@@ -67,29 +68,117 @@ class MyDebugEventCallbacks(DebugEventCallbacks):
             
             if expression_size.value > 1:
                 expression = buffer.value
-                print 'Breakpoint Expression:', expression
+                print 'Expression:', expression
 
-                if -1 != expression.find('CreateFileW'):
-                    debug_client = Bp.GetAdder()
-                    r = Registers(debug_client)
-                    esp = r.get_stack()
-                    logger.debug('[D] esp: ' + hex(esp))
-                    m = DataSpace(debug_client)
-                    data = m.read_memory(esp+4, 4)
-                    addr = struct.unpack('<I', data)[0]
-                    logger.debug('[D] First Parameter Address: ' + hex(addr))
-                    data = m.read_wide_string(addr)
-                    print 'File Created:', data.decode('utf16')
+                debug_client = Bp.GetAdder()
+                r = Registers(debug_client)
+                m = DataSpace(debug_client)
+                esp = r.get_stack()
+                logger.debug('[D] esp: ' + hex(esp))
+                
+                if -1 != expression.find('InternetOpenW'):
+                    
+                    data = m.read_memory(esp+0x04, 4)
+                    param1 = struct.unpack('<I', data)[0]
+                    logger.debug('[D] Parameter1: ' + hex(param1))
+                    data = m.read_wide_string(param1)
+                    print 'Agent:', data.decode('utf16')
+                    
+                    data = m.read_memory(esp+0x0C, 4)
+                    param2 = struct.unpack('<I', data)[0]
+                    logger.debug('[D] Parameter2: ' + hex(param2))
+                    if 0 != param2:
+                        data = m.read_wide_string(param2)
+                        print 'ProxyName:', data.decode('utf16')
+                        
+                    data = m.read_memory(esp+0x10, 4)
+                    param3 = struct.unpack('<I', data)[0]
+                    logger.debug('[D] Parameter3: ' + hex(param3))
+                    if 0 != param3:
+                        data = m.read_wide_string(param3)
+                        print 'ProxyBypass:', data.decode('utf16')
 
+                if -1 != expression.find('InternetConnectW'):
+                    
+                    data = m.read_memory(esp+0x04, 4)
+                    param1 = struct.unpack('<I', data)[0]
+                    logger.debug('[D] Parameter1: ' + hex(param1))
+                    print 'hInternet:', hex(param1)
+                    
+                    data = m.read_memory(esp+0x08, 4)
+                    param2 = struct.unpack('<I', data)[0]
+                    logger.debug('[D] Parameter2: ' + hex(param2))
+                    if 0 != param2:
+                        data = m.read_wide_string(param2)
+                        print 'ServerName:', data.decode('utf16')
+                        
+                    data = m.read_memory(esp+0x0C, 4)
+                    param3 = struct.unpack('<I', data)[0]
+                    logger.debug('[D] Parameter3: ' + hex(param3))
+                    print 'ServerPort:', param3
+
+                    data = m.read_memory(esp+0x10, 4)
+                    param4 = struct.unpack('<I', data)[0]
+                    logger.debug('[D] Parameter4: ' + hex(param4))
+                    if 0 != param4:
+                        data = m.read_wide_string(param4)
+                        print 'Username:', data.decode('utf16')
+
+                    data = m.read_memory(esp+0x14, 4)
+                    param5 = struct.unpack('<I', data)[0]
+                    logger.debug('[D] Parameter5: ' + hex(param5))
+                    if 0 != param5:
+                        data = m.read_wide_string(param5)
+                        print 'Password:', data.decode('utf16')
+                        
+                if -1 != expression.find('HttpOpenRequestW'):
+                    
+                    data = m.read_memory(esp+0x04, 4)
+                    param1 = struct.unpack('<I', data)[0]
+                    logger.debug('[D] Parameter1: ' + hex(param1))
+                    print 'hConnect:', hex(param1)
+                    
+                    data = m.read_memory(esp+0x08, 4)
+                    param2 = struct.unpack('<I', data)[0]
+                    logger.debug('[D] Parameter2: ' + hex(param2))
+                    if 0 != param2:
+                        data = m.read_wide_string(param2)
+                        print 'Verb:', data.decode('utf16')
+                        
+                    data = m.read_memory(esp+0x0C, 4)
+                    param3 = struct.unpack('<I', data)[0]
+                    logger.debug('[D] Parameter3: ' + hex(param3))
+                    if 0 != param3:
+                        data = m.read_wide_string(param3)
+                        print 'ObjectName:', data.decode('utf16')
+                    
+                    data = m.read_memory(esp+0x10, 4)
+                    param4 = struct.unpack('<I', data)[0]
+                    logger.debug('[D] Parameter4: ' + hex(param4))
+                    if 0 != param4:
+                        data = m.read_wide_string(param4)
+                        print 'Version:', data.decode('utf16')
+
+                    data = m.read_memory(esp+0x14, 4)
+                    param5 = struct.unpack('<I', data)[0]
+                    logger.debug('[D] Parameter5: ' + hex(param5))
+                    if 0 != param5:
+                        data = m.read_wide_string(param5)
+                        print 'Referer:', data.decode('utf16')
 
     def LoadModule(self, ImageFileHandle, BaseOffset, ModuleSize, ModuleName, ImageName, CheckSum, TimeDateStamp):
         """LoadModule callback"""
 
         logger.debug('[*] My LoadModule Callback')
+        logger.debug('[D] Module Name: ' + ModuleName)
         try:
-            if -1 != ModuleName.find("KERNEL32"):
-                # set a breakpoint on API CreateFileW
-                self.__pydbgx.set_software_breakpoint_exp('Kernel32!CreateFileW')
+            if -1 != ModuleName.lower().find("wininet"):
+                bpid = self.__pydbgx.set_software_breakpoint_exp(ModuleName + '!InternetOpenW')
+                self.__bplist[bpid] = 'InternetOpenW'
+                bpid = self.__pydbgx.set_software_breakpoint_exp(ModuleName + '!InternetConnectW')
+                self.__bplist[bpid] = 'InternetConnectW'
+                bpid = self.__pydbgx.set_software_breakpoint_exp(ModuleName + '!HttpOpenRequestW')
+                self.__bplist[bpid] = 'HttpOpenRequestW'
         except Exception as e:
             print e
         return DbgEng.DEBUG_STATUS_NO_CHANGE
@@ -127,17 +216,17 @@ if __name__ == '__main__':
     # initialize the debugger
     dbgx = PyDbgX(event_cb=event_callback)
 
-    # bind PyDbgX class to event callbakcs
+    # bind PyDbgX instance to the event callbakcs
     event_callback.bind_pydbgx(dbgx)
 
     # create target process: iexplore.exe
-    # note: can not debug x64 executable if you use 32 bit python
+    # note: can not debug x64 executable with 32 bit python
     dbgx.create_process('c:\\Program Files (x86)\\Internet Explorer\\iexplore.exe', True)
 
-    # active the process so that we can set breakpoints on it
+    # active the process
     dbgx.active_process()
 
-    # set the effective processor to x86 if you use the 64 bit dbgeng.dll
+    # set the effective processor to x86 if the target is a x86 application
     dbgx.set_effective_processor('x86')
     
     # wait for debug event
